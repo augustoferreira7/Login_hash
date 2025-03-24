545b5438a63fa3107cd7a0d8c8a7116da441140d
cj3022501 <cj3022501@LAB01-PC10>
2025-03-24 08:44:26 -0300

    26/03 Adicionar arquivos de projeto.

---
+++ Login_hash/Utilitarios/connection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Login_hash
+{
+    public static class DatabaseHelper
+    {
+        private static string connectionString = @"Server=SQLEXPRESS;Database=hashcj3022501;Integrated Security=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
